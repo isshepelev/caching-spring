b13b8631b20335dd54a5dbb433610b8a88c598c7
ilya <isshepelev2004@gmail.com>
2024-01-09 23:11:28 +0300

    first

---
+++ src/main/java/ru/isshepelev/cachingspring/CachingSpringApplication.java
@@ -0,0 +1,15 @@
+package ru.isshepelev.cachingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class CachingSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CachingSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/isshepelev/cachingspring/CachingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isshepelev.cachingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
